7414f66a1f5b73d15be7a1cf050d643d17a6bfe9
Shaikat <shaikat@shaikats-macbook-pro.local>
2018-04-24 15:36:29 +0600

    shaikat commit

--- FirstAppHakintosh/ViewController.swift
+++ FirstAppHakintosh/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hi It's my first project in hakintosh")
     }
 
     override func didReceiveMemoryWarning() {
